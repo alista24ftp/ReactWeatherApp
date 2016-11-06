23c2742b9917fbcc86470c938002b82275c5de18
alista24ftp <johnlittlefish@hotmail.com>
2016-11-06 14:02:54 -0800

    Prep repo for heroku

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ var express = require('express');
 // Create our app
 var app = express();
 
+// For Heroku use process.env.PORT, but locally since we dont have process.env
+// variable we'll use port 3000
+const PORT = process.env.PORT || 3000;
+
+// For some APIs like openWeatherMap, they only handle http but NOT https requests,
+// so we need to redirect all https requests to http
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next(); // process as normal
+  }else{
+    // redirect to http protocal
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 // Tell it which folder we want to serve
 app.use(express.static('public'));
 
 // Start server
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function(){
+  console.log('Express server is up on port ' + PORT);
 });
