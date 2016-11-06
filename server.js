5a5179aee07b26524ec159a6f96092ef0f30535d
alista24ftp <johnlittlefish@hotmail.com>
2016-11-06 15:13:23 -0800

    Update middleware for localhost

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ const PORT = process.env.PORT || 3000;
 // For some APIs like openWeatherMap, they only handle http but NOT https requests,
 // so we need to redirect all https requests to http
 app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] === 'http'){
-    next(); // process as normal
-  }else{
+  if(req.headers['x-forwarded-proto'] === 'https'){
     // redirect to http protocal
     res.redirect('http://' + req.hostname + req.url);
+  }else{
+    next(); // ie. if it's http, localhost, or anything else, process as normal
   }
 });
 
